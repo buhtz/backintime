f0737f0f61b3198d10864bb0f57df41b8c872338
Christian Buhtz <c.buhtz@posteo.jp>
2023-12-14 11:26:08 +0100

    moved into src folder

--- ENGAGE.sh
+++ ENGAGE.sh
@@ -5,4 +5,15 @@ cd common
 mkdir --parents src/common
 git add src
 git mv *.py src/common/.
+cd ..
 
+# --- GUI (qt) ---
+cd qt
+mkdir --parents src/qt
+git add src
+git mv *.py src/qt/.
+cd ..
+
+# --- clean make file system ---
+git rm common/configure
+git rm qt/configure
